e0fef00d9889050b36d7e74bc2ffc08b76720522
Hunter Sadler <hsadlersemail@gmail.com>
2015-11-13 03:29:04 -0600

    More Stable, Doesn't Run Unless In Raid

--- EarlyPull.lua
+++ EarlyPull.lua
@@ -1,9 +1,20 @@
 -- Auro: Early Pull
--- Version: 1.2.8
+-- Version: 1.4.1
 
--- Trigger [COMBAT_LOG_EVENT_UNFILTERED, ENCOUNTER_START]
-function(event, encounterID, msg, _, srcGUID, srcName, _, _, destGUID, destName, _, _, spellID, spellName)
-  if (event == "ENCOUNTER_START") then
+-- Trigger [COMBAT_LOG_EVENT_UNFILTERED, CHAT_MSG_ADDON]
+function(event, encounterID, msg, _, srcGUID, srcName, _, _, destGUID, destName, _, _, spellID, spellName, flag1, flag2)
+  if (event == "CHAT_MSG_ADDON" and aura_env.addonMsgs[encounterID]) then
+    if (aura_env.addonMsgs[encounterID]) then
+      local subEvent = string.sub(msg, 1, aura_env.addonMsgs[encounterID]["stringLen"]);
+      -- print(subEvent);
+      if (subEvent ~= aura_env.addonMsgs[encounterID]["subEvent"]) then
+        return false;
+      end
+      if (aura_env.debug == true) then
+        print(encounterID .. " Pull Detected");
+      end
+    end
+    if (IsInRaid() == false) then return false; end
     aura_env.pulled = false;
     aura_env.wipeTable(aura_env.raidRoster);
     aura_env.fillRaid(aura_env.raidRoster);
@@ -12,10 +23,13 @@ function(event, encounterID, msg, _, srcGUID, srcName, _, _, destGUID, destName,
     if (event == "COMBAT_LOG_EVENT_UNFILTERED" and aura_env.combatMsgs[msg] and srcName ~= destName and destName and srcName and spellName) then
       if (aura_env.isBoss(srcName) == true) then return false; end
       if (aura_env.isRaidToRaid(srcName, destName, aura_env.raidRoster) == true) then return false; end
+      if (msg == "SPELL_AURA_APPLIED" and flag2 == "BUFF") then return false; end
       if (spellName == -1) then
         spellName = "Melee Swing";
       end
-      print(spellName);
+      if (aura_env.debug == true) then
+        print(spellName);
+      end
       local pull = string.format("%s pulled with %s", srcName or "?", spellName or "?");
       if (aura_env.reporter == true) then
         SendChatMessage(pull, "RAID");
@@ -31,6 +45,9 @@ end
 -- Init
 aura_env.pulled = true;
 aura_env.reporter = false;
+aura_env.debug = false;
+aura_env.lastPull = nil;
+aura_env.lastPullSafeZone = 15;
 aura_env.raidRoster = {};
 aura_env.bosses = {};
 aura_env.combatMsgs = {};
@@ -42,6 +59,13 @@ aura_env.combatMsgs["SWING_DAMAGE"] = true;
 aura_env.combatMsgs["SWING_DAMAGE_LANDED"] = true;
 aura_env.combatMsgs["SWING_MISSED"] = true;
 aura_env.combatMsgs["SPELL_AURA_APPLIED"] = true;
+aura_env.addonMsgs = {};
+aura_env.addonMsgs["BigWigs"] = {};
+aura_env.addonMsgs["BigWigs"]["subEvent"] = "T:BWPull";
+aura_env.addonMsgs["BigWigs"]["stringLen"] = 8;
+aura_env.addonMsgs["D4"] = {};
+aura_env.addonMsgs["D4"]["subEvent"] = "PT";
+aura_env.addonMsgs["D4"]["stringLen"] = 2;
 
 aura_env.isBoss = function(name)
   for i = 1, 10 do
